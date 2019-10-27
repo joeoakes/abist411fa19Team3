e715edf34ccfa30205c2ce8b82397979d9bfca36
Dylan Palaia <dmp5957@psu.edu>
2019-10-27 16:10:05 +0000

    Bug fix for app 2

--- app2.py
+++ app2.py
@@ -7,18 +7,17 @@ import logging
 
 # logging('App2 has begun')
 
-def getJsonFromApp1(): 
-  try:
+try:
     print("create an INET, STREAMing socket using SSL")
     diamondSock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     ssl_sock = ssl.wrap_socket(diamondSock,
                                server_side=True,
                                certfile="server.crt",
                                keyfile="server.key")
-   print("bind the socket to a public host, and a well-known port 8080")
-   ssl_sock.bind(('localhost', 8080))
-   ssl_sock.listen(5)
-   print("ciphers: " + str(ssl_sock.cipher()))
+    print("bind the socket to a public host, and a well-known port 8080")
+    ssl_sock.bind(('localhost', 8080))
+    ssl_sock.listen(5)
+    print("ciphers: " + str(ssl_sock.cipher()))
     while True:
         print("accept connections from outside")
         (clientsocket, address) = ssl_sock.accept()
@@ -27,12 +26,11 @@ def getJsonFromApp1():
         print("Message received")
         print(message)
 
-        logging.info('Message has been recieved by app2')
+        logging.info('Message has been received by app2')
         strJson = message.decode("utf-8")
         print(strJson)
 
- except Exception as e:
+except Exception as e:
     print(e)
     ssl_sock.close()
 
-# logging.info('App2 has ended')
