793a64d027434069f3963f2b99748ed0b73eedd1
Cieara Parker <czp5346@psu.edu>
2019-11-05 02:30:44 +0000

    updated app2

--- app2.py
+++ app2.py
@@ -3,37 +3,43 @@ import ssl
 import json
 import logging
 
+
 # logging.basicConfig(format='%(asctime)s - %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
 
 # logging('App2 has begun')
 def connectionToApp1():
-try:
-    print("create an INET, STREAMing socket using SSL")
-    diamondSock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    ssl_sock = ssl.wrap_socket(diamondSock,
-                               server_side=True,
-                               certfile="server.crt",
-                               keyfile="server.key")
-    print("bind the socket to a public host, and a well-known port 8080")
-    ssl_sock.bind(('localhost', 8080))
-    ssl_sock.listen(5)
-    print("ciphers: " + str(ssl_sock.cipher()))
-    while True:
-        print("accept connections from outside")
-        (clientsocket, address) = ssl_sock.accept()
-        message = clientsocket.recv(1024)
 
-        print("Message received")
-        print(message)
 
-        logging.info('Message has been received by app2')
-        strJson = message.decode("utf-8")
-        print(strJson)
+    try:
+        print("create an INET, STREAMing socket using SSL")
+        diamondSock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        ssl_sock = ssl.wrap_socket(diamondSock,
+                                   server_side=True,
+                                   certfile="server.crt",
+                                   keyfile="server.key")
+        print("bind the socket to a public host, and a well-known port 8080")
+        ssl_sock.bind(('localhost', 8080))
+        ssl_sock.listen(5)
+        print("ciphers: " + str(ssl_sock.cipher()))
+        while True:
+            print("accept connections from outside")
+            (clientsocket, address) = ssl_sock.accept()
+            message = clientsocket.recv(1024)
 
-except Exception as e:
-    print(e)
-    ssl_sock.close()
+            print("Message received")
+            print(message)
+
+            logging.info('Message has been received by app2')
+            strJson = message.decode("utf-8")
+            print(strJson)
+
+    except Exception as e:
+        print(e)
+        ssl_sock.close()
+
 
 def sftpSender():
-    
+
+
 def hmacHasher():
+
