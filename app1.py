201e31aa4e754a17b5ec77e3208549f17cec9458
Cieara Parker <czp5346@psu.edu>
2019-11-05 02:29:05 +0000

    added loop to app1

--- app1.py
+++ app1.py
@@ -13,7 +13,6 @@ import calendar
 import socket
 import ssl
 import json
-import sys
 import urllib.request
 import time
 import logging
@@ -42,9 +41,7 @@ def sendJsontoApp2(JsonToSend):
         ssl_sock = ssl.wrap_socket(diamondSock,
                                    ca_certs="server.crt",
                                    cert_reqs=ssl.CERT_REQUIRED)
-
         ssl_sock.connect(('localhost', 8080))
-
         ssl_sock.send(bytes(JsonToSend, "utf-8"))
         logging.info('DEBUG: Object sent to Application 2')
     except Exception as e:
@@ -53,20 +50,17 @@ def sendJsontoApp2(JsonToSend):
         logging.error('DEBUG: Exception has been thrown')
         ssl_sock.close()
 
-    with open('curlApp1.json', 'w') as outFile:
-        jsonObj = outFile.write(json.dumps(url))
-
-    with open('curlApp1.json', 'r') as json_data:
-        pyObj = json.load(json_data)
+    with open('jsonPayload.txt', 'r') as json_data:
+        pyObj = json.loads(json_data)
         print(repr(pyObj))
 
-        # Description: This method uses CURL to pull JSON payloads from a URL.
-        # Param: None
-        # Returns: Payload from CURL(Bulk or single at a time?)
+    # Description: This method uses CURL to pull JSON payloads from a URL.
+    # Param: None
+    # Returns: Payload from CURL(Bulk or single at a time?)
 
 
 def getJsonPayload():
-    # jsonString = """{"name": "Dylan","lastName": "Palaia","age": 25,"graduated": true, "balance": null}"""
+    # jsonString = """{"name": "Dylan","lastName": "Palaia","age": 25,"graduated": true, "balance": null}"$
 
     url = 'https://jsonplaceholder.typicode.com'
     param = '/post/1/comments'
@@ -82,21 +76,11 @@ def getJsonPayload():
         print(e)
         print("DEBUG: error: %s" % e)
         print("Something went wrong with getting the curl payload")
+
     return JsonPayload
 
 
-# Replacement for curl method
-def getJsonPayloadtwo():
-    try:
-        url = 'https://jsonplaceholder.typicode.com'
-        param = '/posts/1'
-        response = urllib.request.urlopen(url + param)
-        payLoad = response.read()
-
-    except Exception as e:
-        print(e)
-
-    return payLoad  # ======================Below are several time related functions================
+# ======================Below are several time related functions================
 
 
 # Description: A tuple is similar to a list of values.
@@ -141,20 +125,29 @@ def formatDateToHumanReadable():
 # Description: Main Process, Below is a try catch block where all the methods are run.
 # Param: None
 # Returns: None
+
+
 try:
     # Save the Payload to a file.
     # saveJsonTotxtFile()
 
     # Convert the string received by CURL to a Python Object
-    jsonObject = json.loads(getJsonPayload())
-
+    jsonList = json.loads(getJsonPayload())
     # Add the time stamp in seconds to the JSON object.
     # TODO This is where we need to look through the payloads and append the timestamp individually.
+    # Desciption: Loop adds a timestamp to each of the elements in the list
+    # Author: CiearaParker
+    # Param: None
+    # Returns: None
+    for i in jsonList:
+        i['date'] = gettimeinSeconds()
+        print(i)
     # jsonObject['date'] = gettimeinSeconds()
-
     # Print/Show the Json with time stamp in pretty format.
-    print(jsonObject)
+    # print(len(jsonList))
+    # print(jsonList)
+
+
 
 except Exception as e:
     print(e)
-
