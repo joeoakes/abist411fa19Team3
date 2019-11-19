d8d85052b787450fac1d629e4bb981e4b35a0b5f
fjp5090 <fjp5090@psu.edu>
2019-11-19 15:47:57 +0000

    App5 unit test

--- app5Test.py
+++ app5Test.py
@@ -1,1 +1,30 @@
+import logging
+from logging.config import fileConfig
+import socket
 
+
+
+
+
+
+
+serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+serversocket.bind(('localhost', 9999))
+serversocket.listen(5)
+
+
+
+
+
+
+
+
+
+logMessage = logging.config.listen()
+
+
+formatter = logging.Formatter("%(asctime)s;%(levelname)s;%(message)s",
+                              "%Y-%m-%d %H:%M:%S")
+
+
+logging.info("Test")
