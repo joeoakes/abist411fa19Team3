d496f360def6fbdbb78d37b4c2357dd8abd9685e
fjp5090 <fjp5090@psu.edu>
2019-12-13 23:38:06 +0000

    app4 tests for rabbit mq and AES

--- app4Test.py
+++ app4Test.py
@@ -1,1 +1,24 @@
+# Project: Project Diamond
+# Purpose Details: Unit test methods for App 4
+# Course: IST 411
+# Author: Team 1
+# Date Developed: 11/30/19
+# Last Date Changed: 12/13/19
+# Rev:
 
+import app4
+import unittest
+
+class App4RabittMQ(unittest.TestCase):
+	def testMQ(self):
+		test =rabbitMQ
+		self.assertTrue(test)≈ 
+
+
+
+
+
+class App4AESTest(unittest.TestCase):
+    def testAES(self):
+        test = encryptAES 
+        self.assertTrue(test)
