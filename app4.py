83e2d531592bfa6671bb888ca287478592683da4
fjp5090 <fjp5090@psu.edu>
2019-12-13 22:41:27 +0000

    Added AES

--- app4.py
+++ app4.py
@@ -7,13 +7,24 @@
 
 
 import Pyro4
-
+import zlib
+import sys 
 uri = input("What is the Pyro uri of the greeting object?").strip()
 name = input("What is your name?").strip()
 
 greeting_maker = Pyro4.Proxy(uri)
 print(greeting_maker.get_fortune(name))
 
+class AES: 
+	def encryptAES(self, payload) 
+	try: 
 
-
+		pad = b ' '
+		obj = AES.new('This is a key', AES.MOD_CBC, 'This is App4')
+		length = 16 -(len(payload)%16) 
+		payload += length *pad 
+		ciphertext = object.encrypt(payload)
+		return ciphertext
+	except Exception as e: 
+		print (e) 
 
