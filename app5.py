e8f3be83574a60bcfef5ae1fe1e09dd407c4e83e
fjp5090 <fjp5090@psu.edu>
2019-12-13 22:34:43 +0000

    app5.py

--- app5.py
+++ app5.py
@@ -1,3 +1,17 @@
+# Project: Project Diamond
+# Purpose Details: Application five takes the log requests from all the apps and logs them into a database 
+# Course: IST 411
+# Author: Jinal Parmar
+# Date Developed: 10/11/2019
+# Last Date Changed: 11/19/2019
+# Rev: 3.3
+
+
+
+
+
+
+
 import logging
 from logging.config import fileConfig
 
@@ -7,9 +21,8 @@ import pdb
 import socket
 import logging.handlers
 logging.basicConfig(level = logging.INFO, format='%(asctime)s - %(message)s', datefmt='%m/%d/%Y %$')
-
-
-#logMessages = logging.config.listen()
+formatter = logging.Formatter("%(asctime)s;%(levelname)s;%(message)s",
+                              "%Y-%m-%d %H:%M:%S")
 
 
 
@@ -21,9 +34,9 @@ logMessages = logging.config.listen()
 logMessages.start()
 
 
-#Opdb.set_trace()
+
 serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-serversocket.bind(('localhost', 9999))
+serversocket.bind(('localhost', 8080))
 serversocket.listen(5)
 
 
@@ -33,14 +46,16 @@ logger = logging.getLogger('app5')
 
 stream_handler = logging.StreamHandler() 
 stream_handler.setLevel(logging.INFO)
-#pdb.set_trace() 
+
+
+
 logger.addHandler(stream_handler) 
 
 
 
 
 try: 
-	logging.info('Test') 
+	#logging.info('Test') 
 	while True: 
 			(clientsocket,address) = serversocket.accept()
 			logInfo = clientsocket.recv(1024).decode()
