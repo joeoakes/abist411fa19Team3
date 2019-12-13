6f82e121b6da60d54832566fec50b1efe65c01f1
fjp5090 <fjp5090@psu.edu>
2019-12-13 22:48:30 +0000

    AES

--- app4.py
+++ app4.py
@@ -3,9 +3,8 @@
 #Course: IST 411
 #Author: Team 3
 #Date Developed: 11/18/2019
-#Last Date Changed: 11/18/2019
-
-
+#Last Date Changed: 12/13/2019
+from  Crypto.Cipher import AES 
 import Pyro4
 import zlib
 import sys 
@@ -20,7 +19,7 @@ class AES:
 	try: 
 
 		pad = b ' '
-		obj = AES.new('This is a key', AES.MOD_CBC, 'This is App4')
+		obj = AES.new('This is a key', AES.MOD_CBC, 'This is an IV456')
 		length = 16 -(len(payload)%16) 
 		payload += length *pad 
 		ciphertext = object.encrypt(payload)
